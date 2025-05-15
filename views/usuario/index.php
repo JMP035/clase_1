f5edca5c046a25e11bb9ea527583eadf1d3f82bd
jmp035 <jacobmoratayapedroza@gmail.com>
2025-05-15 11:51:03 -0600

    Se realizo la pagina principal de usuarios

---
+++ views/usuario/index.php
@@ -0,0 +1,49 @@
+<h1>HOLA DESDE USUARIOS</h1>
+
+
+
+<form>
+    <div class="mb-3">
+        <input type="hidden" class="form-control" id="usuario_id" name="usuario_id">
+    </div>
+
+    <div class="mb-3">
+        <label for="usuario_nombres" class="form-label">Ingrese sus nombres</label>
+        <input type="text" class="form-control" id="usuario_nombres" name="usuario_nombres">
+    </div>
+
+    <div class="mb-3">
+        <label for="usuario_apellido" class="form-label">Ingreses apellidos</label>
+        <input type="text" class="form-control" id="usuario_apelidos" name="usuario_apellidos">
+    </div>
+
+    <div class="mb-3">
+        <label for="usuario_nit" class="form-label">Ingrese su nombre</label>
+        <input type="hidden" class="form-control" id="usuario_nombres" name="usuario_nombres">
+    </div>
+
+    <div class="mb-3">
+        <label for="usuario_telefono" class="form-label">Ingrese su nombre</label>
+        <input type="hidden" class="form-control" id="usuario_nombres" name="usuario_nombres">
+    </div>
+
+    <div class="mb-3">
+        <label for="usuario_correo" class="form-label">Ingrese su nombre</label>
+        <input type="hidden" class="form-control" id="usuario_nombres" name="usuario_nombres">
+    </div>
+
+    <div class="mb-3">
+        <label for="usuario_estado" class="form-label">Ingrese su nombre</label>
+        <input type="hidden" class="form-control" id="usuario_nombres" name="usuario_nombres">
+    </div>
+    <div class="mb-3">
+        <label for="usuario_situacion" class="form-label">Ingrese su nombre</label>
+        <input type="hidden" class="form-control" id="usuario_nombres" name="usuario_nombres">
+    </div>
+
+
+
+
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
